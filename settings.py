bc50d9c2cbc41f65f7632ffc465768fb9725872e
wasddog <51215035+wasddog@users.noreply.github.com>
2020-12-16 12:30:11 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ board = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
-# boardOnScreen = 27,226,607,806 # if on left side of the screen
-boardOnScreen = 294,261,883,851 # if on full screen
+
+boardOnScreen = 294,261,883,851 # pyautogui.displayMousePosition() to find the pos of the board on screen
 
 WIDTH , HEIGHT = boardOnScreen[2]-boardOnScreen[0], boardOnScreen[3]-boardOnScreen[1]
 cellSize = WIDTH // 9
